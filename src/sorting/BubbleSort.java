afa1d79bc0215f756d941616e4545bd72176c67b
iamrajendra <rajendrarajaram@hotmail.com>
2020-10-16 21:59:20 +0530

    Sorting: Bubble Sort

---
+++ src/sorting/BubbleSort.java
@@ -0,0 +1,51 @@
+package sorting;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class BubbleSort { // Complete the countSwaps function below.
+    static void countSwaps(int[] a) {
+        int count = 0;
+        int temp = 0;
+        int n = a.length;
+        for (int i = 0; i < n; i++) {
+
+            for (int j = 0; j < n - 1; j++) {
+                // Swap adjacent elements if they are in decreasing order
+                if (a[j] > a[j + 1]) {
+                    temp = a[j];
+                    a[j] = a[j + 1];
+                    a[j + 1] = temp;
+                    count++;
+                }
+            }
+
+        }
+        System.out.println("Array is sorted in " + count + " swaps.");
+        System.out.println("First Element: " + a[0]);
+        System.out.println("Last Element: " + a[a.length - 1]);
+    }
+
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        int n = scanner.nextInt();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        int[] a = new int[n];
+
+        String[] aItems = scanner.nextLine().split(" ");
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        for (int i = 0; i < n; i++) {
+            int aItem = Integer.parseInt(aItems[i]);
+            a[i] = aItem;
+        }
+
+        countSwaps(a);
+
+        scanner.close();
+    }
+}
+
